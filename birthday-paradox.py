007db28ab03a45223425bb1ac1423ba77d3ddfa0
Andrew Nicholls <andrewsnicholls@gmail.com>
2019-12-03 06:47:51 -0800

    Update birthday-paradox.py
    Updated problem to use a pandas dataframe. This makes it easier to use the nunique function with axis=1 parameter.

--- birthday-paradox.py
+++ birthday-paradox.py
@@ -6,17 +6,19 @@ At what value of n does that probability exceed 50%?
 '''
 
 import numpy as np
+import pandas as pd
 
 def birthday_problem_simulation(n_people, n_simulations):
     # Assuming a uniform distribution of birthdays and ignoring leap years.
     random_birthdays = np.random.randint(365, size=n_people*n_simulations)
-    birthday_groups = random_birthdays.reshape(n_simulations, n_people)
-    sorted_birthdays = np.sort(birthday_groups)
-    diffs = np.diff(sorted_birthdays, axis=1)
-    all_unique = np.all(diffs, axis=1)
-    # Return the probability of at least 2 people in a group of size 
-    # n_people having a matching birthday.
-    return 1-(np.sum(all_unique)/n_simulations)	
+    # Create a matrix where each row represents a simulation of n_people's birthdays.
+    reshaped_birthdays = random_birthdays.reshape(n_simulations, n_people)
+    # Converting to a dataframe allows us to use the nunique function along each row.
+    df = pd.DataFrame(reshaped_birthdays)
+    # If the number of unique birthdays in a row is less than the number of people in a row then there's been a matching birthday.
+    n_matches = np.sum(df.nunique(axis=1) < n_people)
+    p_match = n_matches/n_simulations
+    return p_match
 
 for n in range(2, 366):
     if birthday_problem_simulation(n_people=n, n_simulations=10**5) > .50:
