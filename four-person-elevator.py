e59126631602e9547a4a51fdd206f373ca2669b5
Andrew Nicholls <andrewsnicholls@gmail.com>
2019-12-20 12:11:24 -0800

    Create four-person-elevator.py

---
+++ four-person-elevator.py
@@ -0,0 +1,16 @@
+'''
+Four Person Elevator
+
+There are four people on the ground floor of a building that has five levels not including the ground floor. 
+They all get into the same elevator. If each person is equally likely to get on any floor and they leave independently 
+of each other, what is the probability that no two passengers will get off at the same floor?
+
+Solving by-hands yields (5*4*3*2)/(5^4) = .192. Let's simulate.
+'''
+
+import numpy as np
+
+n_sims = 10**6
+sims = np.random.randint(5, size=(n_sims, 4))
+n_unique_floors = np.apply_along_axis(lambda x: np.unique(x).size, axis=1, arr=sims)
+np.mean(n_unique_floors == 4) # ~0.19195
