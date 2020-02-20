19ffbe7fa122d3cc319cd86548e74649312dceb7
Andrew Nicholls <andrewsnicholls@gmail.com>
2020-02-20 12:57:30 -0800

    Update monty-hall.py
    Changed `if switch == True:` to `if switching:` since we really want to check `if switching is True:`.

--- monty-hall.py
+++ monty-hall.py
@@ -6,14 +6,14 @@ He then says to you, "Do you want to pick door No. 2?" Is it to your advantage t
 '''
 import numpy as np
 
-def monty_hall(switch=True, n_games=10**7):
+def monty_hall(switching=True, n_games=10**7):
     door_choices = np.random.randint(0, 3, size=n_games)
     door_correct = np.random.randint(0, 3, size=n_games)
 
-    if switch == True:
+    if switching:
         return np.sum(door_choices != door_correct)/n_games
     else: 
         return np.sum(door_choices == door_correct)/n_games
 
-print(monty_hall(switch=True)) # ~.666
-print(monty_hall(switch=False)) # ~.333
+print(monty_hall(switching=True)) # ~.666
+print(monty_hall(switching=False)) # ~.333
