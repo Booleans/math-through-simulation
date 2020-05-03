ef505f797f7293280111687bfc97911e8082bdcc
Andrew Nicholls <andrewsnicholls@gmail.com>
2020-05-03 10:23:45 -0700

    Update flippant-juror.py
    Redid the simulation so it uses numpy instead of the random module. Also cut down lines to a limit of 80 characters to try out PEP8 convention.

--- flippant-juror.py
+++ flippant-juror.py
@@ -3,40 +3,23 @@ Problem #3 in "Fifty Challenging Problems in Probability" by Frederick Mosteller
 
 The Flippant Juror
 
-A three-man jury has two members each of whom independently has a probability p of making the correct decision and
-a third member who flips a coin for each decision (majority rules). A one-man jury has probability p of making the 
-correct decision. Which jury has the better probability of making the correct decision?
+A three-man jury has two members each of whom independently has a probability p
+of making the correct decision and a third member who flips a coin for each 
+decision (majority rules). A one-man jury has probability p of making the 
+correct decision. Which jury has the better probability of making the correct 
+decision?
 
-Solving by hand yields an equal probability for both juries. Let's confirm programmatically. 
+Solving by hand yields an equal probability for both juries. Let's test this
+ with simulation. 
 '''
 
-import random
+import numpy as np
 
-n_sims = 10**7
-p = .80
-n_success_1_person_jury = 0
-n_success_3_person_jury = 0
+# Draw 2 jurors from a binomial distribution with 2 trials and probability p.
+# Then draw one juror from a coin flip.
+jurors_correct = (np.random.binomial(2, p, size=n_sims) + 
+                  np.random.binomial(1, .50, size=n_sims))
+percent_correct = np.mean(jurors_correct >= 2)
 
-for _ in range(n_sims):
-    # Do 3 person jury first.
-    correct_jurors = 0
-    # Evaluate juror 1.
-    if random.random() <= p:
-        correct_jurors += 1
-    # Evaluate juror 2.
-    if random.random() <= p:
-        correct_jurors += 1
-    # Evaluate juror 3, the coin flipper.
-    if random.random() <= .5:
-        correct_jurors += 1
-            
-    if correct_jurors >= 2:
-        n_success_3_person_jury += 1
-    
-    # Look at the 1 person jury.
-    if random.random() <= p:
-        n_success_1_person_jury += 1
-        
-        
-print(n_success_3_person_jury/n_success_1_person_jury) 
-# 1.0003677286257737, they are equally probable. 
+print(percent_correct) 
+# 0.6002154, which is just p, the probability of the one-man jury being correct.
