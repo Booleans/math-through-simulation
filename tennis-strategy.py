4b5252914bd8df56bd622e435d3f145e4937b593
Andrew Nicholls <andrewsnicholls@gmail.com>
2020-05-04 16:50:33 -0700

    Update tennis-strategy.py
    Updated the simulation for the tennis strategy problem to use numpy instead of the random module.

--- tennis-strategy.py
+++ tennis-strategy.py
@@ -3,33 +3,34 @@ Problem #2 in "Fifty Challenging Problems in Probability" by Frederick Mosteller
 
 Successive Wins
 
-To encourage Elmer's promising tennis career, his father offers him a prize if he wins at least (at least) two tennis
-sets in a row in a three-set series to be played with his father and the club champion alternatively:
-father-champion-father, or champion-father-champion, according to Elmer's choice. The champion is a better player
-than Elmer's father. Which series should Elmer choose?
+To encourage Elmer's promising tennis career, his father offers him a prize if 
+he wins at least (at least) two tennis sets in a row in a three-set series to be 
+played with his father (F) and the club champion (C) alternatively: 
+father-champion-father, or champion-father-champion, according to Elmer's choice.
+The champion is a better player than Elmer's father. Which series should Elmer 
+choose?
 
-I've worked this out by hand but let's simulate to confirm that champion-father-champion really is the superior strategy.
+I've worked this out by hand but let's simulate to confirm that C-F-C really is 
+the superior strategy.
 '''
 
-import random
+import numpy as np
 
-p_win = {'F':.40, 'C':.30}
-n_wins = {'FCF':0, 'CFC':0}
+p_father = .40
+p_champ  = .30
 n_sims = 10**7
-player_sequences = ('FCF', 'CFC')
 
-for sequence in player_sequences:
-    for _ in range(n_sims):
-        previous_win = False
-        for player in sequence:
-            if random.random() <= p_win[player]:
-                if previous_win == True:
-                    n_wins[sequence] += 1
-                    break
-                else:
-                    previous_win = True
-            else:
-                previous_win = False
+p_FCF = [p_father, p_champ, p_father]
+p_CFC = [p_champ, p_father, p_champ]
 
-print(n_wins['CFC']/n_wins['FCF'])
-# Yields ~1.0632, meaning more games are won if Elmer chooses the C-F-C sequence of games.
+games = np.random.binomial(n=1, p=p_FCF, size=(n_sims, 3))
+# Elmer wins by winning the middle game and at least 2 total games.
+wins = (games[:, 1] == 1) & (np.sum(games, axis=1) >= 2)
+win_pct_fcf = np.mean(wins)
+
+games = np.random.binomial(n=1, p=p_CFC, size=(n_sims, 3))
+wins = (games[:, 1] == 1) & (np.sum(games, axis=1) >= 2)
+win_pct_cfc = np.mean(wins)
+
+print(win_pct_cfc/win_pct_fcf)
+# Yields ~1.063, meaning a higher proportion of C-F-C games were won.
