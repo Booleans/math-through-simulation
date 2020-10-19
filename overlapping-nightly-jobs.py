7d54b899b74cbe9c9f3bc1705b3a4ac08b2b52d7
Andrew Nicholls <andrewsnicholls@gmail.com>
2020-10-19 09:22:15 -0700

    Updated Overlapping Nightly Jobs
    Changed increments of time from seconds to milliseconds for a more precise answer. Also deleted the Fifty Challenging Problems in Probability book from the repo.

--- overlapping-nightly-jobs.py
+++ overlapping-nightly-jobs.py
@@ -1,13 +1,15 @@
-# Every night between 7pm and midnight, two computing jobs from two different sources are randomly started with each one lasting an hour.
-# Unfortunately, when the jobs simultaneously run, they cause a failure in some of the other company’s nightly jobs, resulting in downtime for the company that costs $1,000. 
+# Every night between 7 PM and midnight, two computing jobs from two different sources are randomly started with each one lasting an hour.
+# Unfortunately, when the jobs simultaneously run, they cause a failure in some of the other company’s nightly jobs, 
+# resulting in downtime for the company that costs $1,000. 
 
 # The CEO needs you to tell her the annual cost of this issue. 
 
 import numpy as np
 
-# Simulate the second that each process starts on. We could be more precise if we went down to smaller increments of time.
-process_1 = np.random.randint(0, 5*60*60, size=10**7)
-process_2 = np.random.randint(0, 5*60*60, size=10**7)
-# Each job is an hour long so there's an overlap if their starting times are within 3,600 seconds of each other.
-overlap_percentage = np.mean(np.abs(process_1 - process_2) <= 3_600)
+# Simulate the microsecond that each process starts on. We could be more precise if we went down to smaller increments of time.
+# Between 7 PM and midnight there are 5 hours, or 5*60*60*1000 microseconds that each job could be starting on.
+process_1 = np.random.randint(0, 5*60*60*1000, size=10**7)
+process_2 = np.random.randint(0, 5*60*60*1000, size=10**7)
+# Each job is an hour long so there's an overlap if their starting times are within 3,600,000 milliseconds of each other.
+overlap_percentage = np.mean(np.abs(process_1 - process_2) <= 3_600_000)
 annual_cost = overlap_percentage * 365 * 1000
