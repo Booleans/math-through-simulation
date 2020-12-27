6c96c5983f369e4f968ee986ccd67c99dd5ba93b
Andrew Nicholls <andrew.s.nicholls@gmail.com>
2020-12-26 19:41:16 -0500

    Update overlapping-nightly-jobs.py
    Changed comments to accurately reflect that my code uses milliseconds, not microseconds.

--- overlapping-nightly-jobs.py
+++ overlapping-nightly-jobs.py
@@ -6,8 +6,8 @@
 
 import numpy as np
 
-# Simulate the microsecond that each process starts on. We could be more precise if we went down to smaller increments of time.
-# Between 7 PM and midnight there are 5 hours, or 5*60*60*1000 microseconds that each job could be starting on.
+# Simulate the millisecond that each process starts on. We could be more precise if we went down to smaller increments of time.
+# Between 7 PM and midnight there are 5 hours, or 5*60*60*1000 milliseconds that each job could be starting on.
 process_1 = np.random.randint(0, 5*60*60*1000, size=10**7)
 process_2 = np.random.randint(0, 5*60*60*1000, size=10**7)
 # Each job is an hour long so there's an overlap if their starting times are within 3,600,000 milliseconds of each other.
