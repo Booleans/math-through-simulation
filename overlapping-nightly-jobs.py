4e54cb933a6a5786e316869a3be8aa8ab7d37777
Andrew Nicholls <andrew.s.nicholls@gmail.com>
2021-05-27 19:30:54 -0700

    Update overlapping-nightly-jobs.py
    Instead of generating the millisecond in the interval that each job starts on, let's just choose a number between 0 and 1, representing where in the interval each job starts.

--- overlapping-nightly-jobs.py
+++ overlapping-nightly-jobs.py
@@ -5,11 +5,12 @@
 # The CEO needs you to tell her the annual cost of this issue. Write a simulation to approximate the annual cost. 
 
 import numpy as np
-
-# Simulate the millisecond that each process starts on. We could be more precise if we went down to smaller increments of time.
-# Between 7 PM and midnight there are 5 hours, or 5*60*60*1000 milliseconds that each job could be starting on.
-process_1 = np.random.randint(0, 5*60*60*1000, size=10**7)
-process_2 = np.random.randint(0, 5*60*60*1000, size=10**7)
-# Each job is an hour long so there's an overlap if their starting times are within 3,600,000 milliseconds of each other.
-overlap_percentage = np.mean(np.abs(process_1 - process_2) <= 3_600_000)
+# There are five hours in this interval and each job lasts one hour, or 20% of the total interval.
+# Let's use np.random.random to generate a number between 0 and 1, representing the position in the interval that the jobs start.
+# If the difference between start times is <= 20% of the interval then there's an overlap. 
+process_1 = np.random.random(size=10**7)
+process_2 = np.random.random(size=10**7)
+overlap_percentage = np.mean(np.abs(process_1 - process_2) <= 0.20)
 annual_cost = overlap_percentage * 365 * 1000
+
+print(annual_cost)
